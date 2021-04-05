e625235d00ccb593238d4bc59eedd62d8148dbd4
VikasTS <vikasone123@gmail.com>
2021-04-05 20:20:15 +0530

    initial

---
+++ Sample/src/org/learn/sample/Sample.java
@@ -0,0 +1,11 @@
+package org.learn.sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("it's a sample project");
+
+	}
+
+}
